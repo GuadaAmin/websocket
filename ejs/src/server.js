355186c647d6928ef5e0c4a1e7c1ed783ea2f476
guadaamin <guaddamin@gmail.com>
2022-11-14 12:13:11 -0300

    v3

--- ejs/src/server.js
+++ ejs/src/server.js
@@ -1,29 +1,43 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
+const ProductosContainer = require('../productos.js');
+const productos = new ProductosContainer();
 
-const personas = [];
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
+let form = Boolean(true)
 
 app.get('/', (req, res) => {
-    res.render('inicio', { personas });
-});
+  form = true;
+  res.render('../views/main.ejs', {form}); 
+})
 
-app.post('/personas', (req, res) => {
-    
-    personas.push(req.body);
-    console.log(personas);
-    res.redirect('/');
-});
+app.get('/productos', (req, res) => {
+  form = false;
+  const getProductos = productos.getAll();
+  res.render('../views/main.ejs', {getProductos, form});
+})
+app.post('/', async (req, res) => {
+  form = true;
+  const { nombre, precio, url } = req.body;
+  const newProduct = await productos.save(nombre, precio, url);
+  res.json('../views/main.ejs', {newProduct, form});
+  // const { body } = req;
+  // const newProduct = productos.save(body);
+  // res.render(newProduct, {form});
+})
 
 const port = 8080;
+
 const server = app.listen(port, () => {
-    console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+  console.log(`Servidor http escuchando en el puerto http://localhost:${port}`);
 });
+
 server.on('error', error => {
   console.log('Error en servidor', error);
 });
