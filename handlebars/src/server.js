3f6a9e887507558b91d00f0966093e92e4b27acb
guadaamin <guaddamin@gmail.com>
2022-11-14 13:16:51 -0300

    v5

--- handlebars/src/server.js
+++ handlebars/src/server.js
@@ -3,28 +3,31 @@ const exphbs = require('express-handlebars');
 const app = express();
 const ProductosContainer = require('../productos.js');
 const productos = new ProductosContainer();
-//const router = express.Router()
 
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
-app.set('views', 'views');
+app.set('views', './views');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-//app.use('/productos', router)
+
+let form = Boolean(true)
 
 app.get('/', (req, res) => {
-  res.render('../views/form'); 
+  form = true;
+  res.render('form'); 
 })
 
 app.get('/productos', (req, res) => {
+  form = false;
   const getProductos = productos.getAll();
-  res.render('../views/table', {getProductos});
+  res.render('table', {getProductos});
 })
 app.post('/', (req, res) => {
+  form = true;
   const { nombre, precio, url } = req.body;
   const newProduct = productos.save(nombre, precio, url);
-  res.render('../views/table', {newProduct});
+  res.render('table', {newProduct});
 })
 
 const port = 8080;
--- pug/src/server.js
+++ pug/src/server.js
@@ -10,18 +10,27 @@ app.set('views', 'views');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+let form = Boolean(true)
+
 app.get('/', (req, res) => {
-  res.render('../views/index.pug'); 
+  form = true;
+  res.render('../views/main.pug', {form}); 
 })
 
 app.get('/productos', (req, res) => {
+  form = false;
   const getProductos = productos.getAll();
-  res.render('../views/index.pug', {getProductos});
+  res.render('../views/main.pug', {getProductos, form});
 })
 app.post('/', (req, res) => {
-  const { nombre, precio, url } = req.body;
-  const newProduct = productos.save(nombre, precio, url);
-  res.render('../views/index.pug', {newProduct});
+  form = true;
+  // const { nombre, precio, url } = req.body;
+  // const newProduct = productos.save(nombre, precio, url);
+  // res.json({newProduct});
+  // res.render('../views/main.pug', {form})
+  const { body } = req;
+  const newProduct = productos.save(body);
+  res.json(newProduct, {form});
 })
 
 const port = 8080;
